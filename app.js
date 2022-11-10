53eba1ff338c65580c93644e34bcd8d6f53ba405
Ronaldo Lipata <lipata.ronaldo@gmail.com>
2022-11-10 23:36:33 +0800

    feat: add log and install dotenv
    - add console.log from app.listen
    - install dotenv package and used dotenv.config() to app.js
    - add .env to .gitignore

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import process from 'node:process';
+import dotenv from 'dotenv';
 
 const app = express();
+
+dotenv.config();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`App is listening to port ${PORT}`);
+});
