452b258f34e7fc1f2c14d2a98a5dcf84f4970513
Ronaldo Lipata <lipata.ronaldo@gmail.com>
2022-11-13 21:51:05 +0800

    feat: add '/' endpoint

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.status(200).json({
+    Message: 'Welcome to Memoirs!',
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`App is listening to port ${PORT}`);
 });
