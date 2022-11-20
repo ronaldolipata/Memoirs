bb53c0e66e6f2cb15ebaeec00ed49bbcb4ab4e6b
Ronaldo Lipata <lipata.ronaldo@gmail.com>
2022-11-20 23:19:55 +0800

    feat: add endpoint and middlewares for post creation - add posts route - add post fields validation - add id validation

---
+++ middleware/idValidation.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import User from '../models/User.js';
+
+const idValidation = async (req, res, next) => {
+  const authorId = req.header('X-USER-ID');
+  const { Types } = mongoose;
+
+  // Check if valid ObjectId
+  if (!Types.ObjectId.isValid(authorId)) {
+    return res.status(422).json({
+      Error: 'Invalid ID',
+    });
+  }
+
+  const idExists = await User.findOne({
+    _id: authorId,
+  });
+
+  if (!idExists) {
+    return res.status(404).json({
+      Error: 'No User found',
+    });
+  }
+
+  next();
+};
+
+export default idValidation;
